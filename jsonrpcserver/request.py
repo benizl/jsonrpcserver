99b034617e3b91644dfa9101646ba566c53b81ff
Ben Nizette <benjamin.nizette@anu.edu.au>
2016-04-13 10:35:39 +1000

    Request: Allow user-defined parameter munging before dispatch

--- jsonrpcserver/request.py
+++ jsonrpcserver/request.py
@@ -158,6 +158,11 @@ class Request(object):
     #: converting messy camelCase methods and param names. I always enable this.
     convert_camel_case = False
 
+    #: Function with signature args, kwargs = function(args, kwargs) that normalises
+    #: the argument list in an application-specific way. Useful for munging datatypes
+    #: etc for situations where you can't change the exported function signature
+    arg_norm_function = None
+
     def __init__(self, request):
         """
         :param request: JSON-RPC request, in dict or string form
@@ -170,6 +175,9 @@ class Request(object):
         self.method_name = request['method']
         # Get arguments from the request, if any
         self.args, self.kwargs = _get_arguments(request)
+        # Normalize, if the user has supplied such a function
+        if self.arg_norm_function:
+            self.args, self.kwargs = self.arg_norm_function(self.args, self.kwargs)
         # Get request id, if any
         self.request_id = request.get('id')
         # Convert camelCase to underscore
